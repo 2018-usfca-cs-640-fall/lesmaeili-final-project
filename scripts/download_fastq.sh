37bd0fa1731dd3622518b506866a256e513a4a98
Leila Esmaeili <lesmaeili@dons.usfca.edu>
2018-11-26 17:23:00 -0800

    Fixing Shellcheck Errors Bash Script

--- scripts/download_fastq.sh
+++ scripts/download_fastq.sh
@@ -12,6 +12,6 @@ export PATH=$PATH:/Users/leilaesmaeili/sratoolkit.2.9.2-mac64/bin
 # exclude first line
 for SRA_number in $(cut -f 10 data/metadata/SraRunTable.txt | tail -n +2)
 do
-   echo Downloading $SRA_number 
-   fastq-dump -v "$SRA_number" -O data/raw_data
+   echo Downloading "$SRA_number"
+   fastq-dump -v "SRA_number"-O data/raw_data
 done
